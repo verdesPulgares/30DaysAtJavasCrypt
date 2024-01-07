c6ef90517764f3f873520316f0e7eafb5de3eb6e
verdePulgares <verdespulgares@gmail.com>
2024-01-06 21:21:58 -0300

    day 06 pushing

---
+++ day06/scriptDay06.js
@@ -0,0 +1,82 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+//1
+for (let i = 0; i <= 10; i++) {
+  console.log(i)
+};
+
+//
+let ii = 0
+while (ii <= 10) {
+  console.log(ii)
+  ii++
+};
+
+//
+let iii = 0
+do {
+  console.log(iii)
+  iii++
+} while (iii <= 10) ;
+
+//2
+
+
+for (let iI= 10; iI >= 0; iI--) {
+  console.log(iI)
+};
+
+//
+let iiI = 10
+while (iiI >= 0) {
+  console.log(iiI)
+  iiI--
+};
+
+//
+let iiiI = 10
+do {
+  console.log(iiiI)
+  iiiI--
+} while (iiiI >= 0) ;
+
+//3
+let n = 7;
+for (let i = 0; i <= n; i++) {
+  console.log(i)
+};
+
+//4
+let repeatingString = '#'
+for (let i = 0; i <= 7; i++) {
+  console.log(repeatingString.repeat(i))
+};
+
+//5
+for (let i = 0; i <= 10; i++) {
+  console.log(i + " x " + i + " = " + (i * i))
+};
+
