e8d3b2865832c27461a23de3628aa1e34ec79349
verdePulgares <verdespulgares@gmail.com>
2024-01-12 17:22:29 -0300

    getting thru

--- day06/scriptDay06.js
+++ day06/scriptDay06.js
@@ -288,6 +288,8 @@ console.log(randomRGBColor);
 
 //4
 //debugger
+
+
 let upperCaseCountries = [];
 for (let i = 0; i <= countries.length - 1; i++) {
   upperCaseCountries.push(countries[i].toUpperCase())
@@ -304,18 +306,218 @@ for (let i = 0; i < upperCaseCountries.length; i++) {
 console.log(lengthArray);
 
 //6
+
 //debugger
+
+countries.push('Iceland');
+upperCaseCountries.push('ICELAND');
+
+console.log(countries);
 let arrayOfArrays = [];
+
 for (let i = 0; i < countries.length; i++) {
   let insiderArray = [];
   insiderArray.push(countries[i], upperCaseCountries[i].slice(0,3), lengthArray[i])
   arrayOfArrays.push(insiderArray)
 };
+
 console.log(arrayOfArrays);
+
 
 //7
 //debugger
+let landCountries = countries.filter((word) => word.includes('land'));
+console.log(landCountries);
+
+//8
+let iaEndingCountries = [];
+function areThereCountriesEndingWithIa() {
+  for (let i = 0; i < countries.length; i++) {
+    if (countries[i].endsWith('ia')) {
+      iaEndingCountries.push(countries[i])
+    } 
+  };
+  
+  if (iaEndingCountries.length === 0) {
+    console.log('there are no countries ending with "ia"')
+  } else {
+    console.log(iaEndingCountries)
+  }
+};
+
+areThereCountriesEndingWithIa();
+
+//9
+debugger
+let longestWord = '';
+function findLongestWord (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > longestWord.length) {
+      longestWord = arr[i];
+    }
+  }
+  return longestWord
+};
+
+console.log(findLongestWord(countries));
+
+//10
+let countriesWith5Characters = [];
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].length == 5) {
+    countriesWith5Characters.push(countries[i])
+  }
+};
+console.log(countriesWith5Characters);
+
+//11
+//debugger;
+let longestNamedWebTech = '';
+
+function findLongestWebTech () {
+  for (let i = 0; i < webTechs.length; i++) {
+    if (webTechs[i].length > longestNamedWebTech.length) {
+      longestNamedWebTech = webTechs[i];
+    }
+  }
+  return longestNamedWebTech
+}
+console.log(findLongestWebTech());
+
+//12
+let webTechsArray = []
+for (let i = 0; i < webTechs.length; i++) {
+  let innerWebTechArray = [];
+  innerWebTechArray.push(webTechs[i], webTechs[i].length)
+  webTechsArray.push(innerWebTechArray)
+};
+console.log(webTechsArray);
+
+//13 this bad boy didnt stress me at all
+//debugger
+let mernAcronym = [];
+for (let i = 0; i < mernStack.length; i++) {
+  let firstCharacter = mernStack[i].slice(0,1)  
+  mernAcronym.push(firstCharacter)
+};
+
+let mernAcronymButItsAString = mernAcronym.join('')
+
+console.log(mernAcronymButItsAString); 
+
+//14
+const webTechs2 = ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"];
+for (let i = 0; i < webTechs2.length; i++) {
+  console.log(webTechs2[i])
+};
+
+for (const element of webTechs2) {
+  console.log(element.toUpperCase()) //just added the toUpperCase to print it out differently in the console.
+};
+
+//15
+//debugger
+let fruitArray = ['banana', 'orange', 'mango', 'lemon'];
+let reversedFruitArray = [];
+for (let i = fruitArray.length -1; i >= 0; i--) {
+  reversedFruitArray.push(fruitArray[i]);
+}
+console.log(fruitArray);
+console.log(reversedFruitArray);
+
+
+//16
+//debugger;
 /*
+const frontEnd = [webTechs2.slice(0,4)];
+const backEnd = [webTechs2.slice(5, 8)]
+const fullStackk = frontEnd + backEnd;
+console.log(fullStackk);
+
+console.log('const fullStack = [\n\t[' + fullStackk[0] + '],\n\t[' + fullStackk[1] + '\n]');
+
+console.log('const fullStack = [\n\t[' + frontEnd + '],\n\t[' + backEnd + ']\n]');
+console.log(frontEnd);
+console.log(backEnd);
+console.log(upperCaseCountries);
+*/
+//it seems like the assignment was missunderstood, let's go again:
+
+//debugger;
+const fullStack = [
+  ['HTML', 'CSS', 'JS', 'React'],
+  ['Node', 'Express', 'MongoDB']
+];
+function consoleameLaLoganiza () {
+  for (let i = 0; i < fullStack.length; i++) {
+    for (const element of fullStack[i]) {
+      console.log(element.toUpperCase())
+    }
+  }
+};
+consoleameLaLoganiza();
+
+//excercises level 3
+//1
+//debugger
+const copiedCountries = countries.slice();
+console.log(copiedCountries);
+console.log(countries);
+
+
+//2
+//debugger
+const sortedCopyOfCountries = copiedCountries.sort();
+console.log(sortedCopyOfCountries);
+
+//3
+console.log(webTechs.sort(), mernStack.sort());
+
+//4 
+import { importedCountries } from "./importedCountries.js";
+console.log(importedCountries);
+
+
+//debugger
+
+let countriesWith4Characters = importedCountries.filter((word) => word.includes('land'));;
+console.log(countriesWith4Characters);
+
+//5
+debugger
+let longestImportedWord = '';
+function findLongestImportedWord (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > longestImportedWord.length) {
+      longestImportedWord = arr[i];
+    }
+  }
+  return longestImportedWord
+};
+
+findLongestImportedWord(importedCountries);
+
+//7 countries.filter((word) => word.includes('land'));
+
+
+
+/* cemetery of processes and fallen intents
+
+for (let i = 0; i < countries.length; i++) {
+
+}
+
+
+let arrayLand = countries.filter((word) => word.includes('land'));
+console.log(arrayLand)
+console.log(arrayLand)
+console.log(countries);
+console.log(arrayOfArrays)
+
+let landArray = []
+for (let i = 0; i < arrayOfArrays.length; i++) {
+  arrayOfArrays[i].forEach(filter((word) => word.matches('land'))) }
+  
 let landArray = arrayOfArrays.filter(item => item.includes('land'));
 function lookingForLand() {
   for (let i = 0; i < arrayOfArrays.length; i++) {
@@ -326,11 +528,45 @@ function lookingForLand() {
 };
 lookingForLand();
 console.log(landArray);
-*/
-console.log(arrayOfArrays[0])
-/*debugger
+console.log(arrayOfArrays)
 
-let landArray = []
-for (let i = 0; i >arrayOfArrays.length; i++) {
-  arrayOfArrays[i].forEach()
-}*/
+console.log(arrayOfArrays[5][0].includes('land'));
+
+arrayOfArrays[5][0].includes('land') ? console.log('yes') : console.log('no')
+
+//debugger
+
+let landArray = [];
+function filterItems(arr) {
+  landArray.push(arr.filter((item) => item.includes('land')));
+};
+console.log(filterItems(arrayOfArrays));
+
+//debugger
+
+for (let i = 0; i > arrayOfArrays.length; i++) { 
+  for (let ii = 0; ii > 3; ii++) {
+  if (arrayOfArrays[i][ii].includes('land')) {
+    landArray.push(arrayOfArrays[i][ii])
+  }
+  };
+};
+
+//debugger
+
+for (let i = 0; i > arrayOfArrays.length; i++) { 
+  for (let ii = 0; ii > 3; ii++) {
+  console.log(arrayOfArrays[i][ii])
+  };
+};
+
+const targetLand = 'land';
+
+const landArray = arrayOfArrays.reduce((accumulator, innerArray) => {
+  let filteredWords = innerArray.filter(currentValue => currentValue.includes(targetLand));
+  return accumulator.concat(filteredWords);
+}, []);
+
+console.log(landArray);
+
+*/
