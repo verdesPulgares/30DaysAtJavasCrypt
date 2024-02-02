f26ec583cc7129bbd249f2a00e2ed4608623c8e7
verdePulgares <verdespulgares@gmail.com>
2024-02-02 09:06:33 -0300

    Done with day 10. lets speed this up a

--- day10/scriptDay10.js
+++ day10/scriptDay10.js
@@ -33,5 +33,56 @@ const countries = [
     ['Sweden', 'Stockholm'],
     ['Norway', 'Oslo'],
   ]
-const map = new Map(countries);
-console.log(map)
+const countriesAndTheirLengthsMap = new Map();
+countries.forEach(([country]) => {
+  countriesAndTheirLengthsMap.set(country, country.length);
+});
+console.log(countriesAndTheirLengthsMap)
+
+//Exercises: Level 2:
+//1
+let aBUnion = [...a, ...b]
+let abUnionSet = new Set(aBUnion);
+console.log(a, b, abUnionSet)
+
+//2
+let A = new Set(a);
+let B = new Set(b)
+let abIntersection = a.filter((item) => B.has(item));
+let C = new Set(abIntersection);
+console.log(C)
+
+//3 "Find a with b". assignment not clear
+
+//Exercise: Level 3:
+//debugger
+import { countries_data } from "./countriesData.js";
+function countLanguages(countries_data) {
+  const allLanguages = countries_data.flatMap(country => country.languages);
+  const uniqueLanguages = Array.from(new Set(allLanguages));
+  return uniqueLanguages.length
+}
+const languageCount = countLanguages(countries_data)
+console.log(`There are a total of ${languageCount} unique languages`);
+
+//debugger;
+function topSpokenLanguages(countriesData, topX) {
+  const languageCountMap = new Map();
+
+  countriesData.forEach(country => {
+    country.languages.forEach(language => {
+      languageCountMap.set(language, (languageCountMap.get(language) || 0) + 1);
+    });
+  });
+
+  const sortedLanguages = Array.from(languageCountMap.entries())
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, topX)
+    .map(([language, count]) => ({ language, count }));
+  
+  return sortedLanguages;
+};
+
+const topLanguages = topSpokenLanguages(countries_data, 3)
+console.log(topSpokenLanguages(countries_data, 7))
+
